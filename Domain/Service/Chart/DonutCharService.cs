90c5eca8dc7ff9cf5081c92e2440d311a28204dc
RK3nshin <oliveiraguilbert@gmail.com>
2025-08-05 00:52:04 -0300

    feat - página de scraping , menu lateral e darkmode

--- Domain/Service/Chart/DonutCharService.cs
+++ Domain/Service/Chart/DonutCharService.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Components;
 
 namespace DashboardTrilhaEsporte.Domain.Service{
-    public class DonutCharService : ComponentBase{
+    public class DonutCharService : ComponentBase
+    {
 
-    [Parameter] public double[] Data { get; set; } = Array.Empty<double>();
-    [Parameter] public string[] Labels { get; set; } = Array.Empty<string>();
-    [Parameter] public string Width { get; set; } = "300px";
-    [Parameter] public string Height { get; set; } = "300px";
-    [Parameter] public string Title { get; set; } = "Total";
+        [Parameter] public double[] Data { get; set; } = Array.Empty<double>();
+        [Parameter] public string[] Labels { get; set; } = Array.Empty<string>();
+        [Parameter] public string Width { get; set; } = "300px";
+        [Parameter] public string Height { get; set; } = "300px";
+        [Parameter] public string Title { get; set; } = "Total";
 
     }
 }
